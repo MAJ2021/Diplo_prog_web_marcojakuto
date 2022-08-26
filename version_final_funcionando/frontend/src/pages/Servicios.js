bf71afb970c2739f7425e0c7223d3ab5a547af98
MAJ <marco.jakuto@gmail.com>
2022-08-25 23:49:22 -0300

    trabajo final funcionando 100%

---
+++ version_final_funcionando/frontend/src/pages/Servicios.js
@@ -0,0 +1,92 @@
+
+import '../styles/Servicios.css';
+
+const Servicios = (props) => {
+
+    return (
+        <main className="servicio">
+
+            <div className="servicio_titulo">
+                <h2>
+                    Nuestros Servicios
+                </h2>
+            </div>
+            <div className="servicio_contenedor">
+                <div className="contenedor_foto">
+                    <img src="img/instrumentacion_electricidad.jpeg" alt="foto
+                    instrumentacion y electricidad"/>
+                </div>
+                <div className="contenedor_txt">
+                    <h3>Instrumentación & Electricidad Industrial </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Nostrum iusto explicabo eius pariatur consequuntur,
+                        ipsam, nulla numquam beatae rerum, minus cupiditate
+                        magnam animi reiciendis fugit! Omnis maxime earum illum
+                        facere? Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Consequuntur cumque cum atque quasi
+                        quis iure architecto et, voluptatum dolorum obcaecati,
+                        ipsum enim impedit. Voluptates esse iste error magni
+                        quaerat modi!</p>
+                </div>
+            </div>
+
+            <div className="servicio_contenedor">
+                <div className="contenedor_foto">
+                    <img src="img/automatizacion.jpeg" alt="foto
+                    instrumentacion & electricidad"/>
+                </div>
+                <div class="contenedor_txt">
+                    <h3>Automatización & Control de Procesos</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Nostrum iusto explicabo eius pariatur consequuntur,
+                        ipsam, nulla numquam beatae rerum, minus cupiditate
+                        magnam animi reiciendis fugit! Omnis maxime earum illum
+                        facere? Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Consequuntur cumque cum atque quasi
+                        quis iure architecto et, voluptatum dolorum obcaecati,
+                        ipsum enim impedit. Voluptates esse iste error magni
+                        quaerat modi!</p>
+                </div>
+            </div>
+            <div className="servicio_contenedor">
+                <div className="contenedor_foto">
+                    <img src="img/sistemas_env.jpg" alt="foto
+                    instrumentacion & electricidad"/>
+                </div>
+                <div className="contenedor_txt">
+                    <h3>Desarrollo de prototipos & Sistemas Embebidos</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Nostrum iusto explicabo eius pariatur consequuntur,
+                        ipsam, nulla numquam beatae rerum, minus cupiditate
+                        magnam animi reiciendis fugit! Omnis maxime earum illum
+                        facere? Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Consequuntur cumque cum atque quasi
+                        quis iure architecto et, voluptatum dolorum obcaecati,
+                        ipsum enim impedit. Voluptates esse iste error magni
+                        quaerat modi!</p>
+                </div>
+            </div>
+
+            <div className="servicio_contenedor">
+                <div className="contenedor_foto">
+                    <img src="img/programacion.jpeg" alt="foto
+                    instrumentacion & electricidad"/>
+                </div>
+                <div className="contenedor_txt">
+                    <h3>Desarrollo de prototipos & Sistemas Embebidos</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Nostrum iusto explicabo eius pariatur consequuntur,
+                        ipsam, nulla numquam beatae rerum, minus cupiditate
+                        magnam animi reiciendis fugit! Omnis maxime earum illum
+                        facere? Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Consequuntur cumque cum atque quasi
+                        quis iure architecto et, voluptatum dolorum obcaecati,
+                        ipsum enim impedit. Voluptates esse iste error magni
+                        quaerat modi!</p>
+                </div>
+            </div>
+        </main>
+
+    );
+}
+export default Servicios; 
