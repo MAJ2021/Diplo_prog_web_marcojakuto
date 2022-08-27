2b0e0a4e5b5365b58d0b9160d7d3ea94acb0de2e
MAJ <marco.jakuto@gmail.com>
2022-08-27 11:31:51 -0300

    trabajo terminado

---
+++ Entrga_final/frontend/src/pages/Maj.js
@@ -0,0 +1,17 @@
+
+import '../styles/Maj.css';
+
+const Maj = (props) => {
+
+  return (
+
+
+    <div className="foto_pag_inicial_1">
+
+      <img src="img/imagen_34.jpg" alt="foto_pag_inicial" className="foto_1" />
+
+    </div>
+
+  );
+}
+export default Maj;
